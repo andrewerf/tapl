1915b59ede6ea263f96b9be3b36c78dee1f481dd
Andrew <andrew-aralov@yandex.ru>
2022-09-03 15:31:23 +0300

    File parsing in main and arithmetics written in pure lambda.

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,34 @@
 module Main (main) where
 
-import TypelessArithmetics
-import TypelessArithmeticsParser ( parse )
+import TypelessLambda
+import TypelessLambdaAST
+import TypelessLambdaParser ( parse )
+import System.Environment.Blank ( getArgs )
+
+
+processString :: Context -> String -> Maybe Term
+processString ctx s = ( removeNames ctx.desugar.parse ) s >>= eval
 
 main :: IO ()
-main =
-  lines <$> getContents >>= mapM_ ( print.eval.parse )
+main = do
+  args <- getArgs
+  let ctx = tail args
+  fileContent <- readFile $ head args
+  print $ TermWithContext ctx <$>  processString ctx fileContent
+
+
+--main :: IO ()
+--main =
+--  print term >>
+--  print desugaredTerm >>
+--  print maybeNamelessTerm >>
+--  print isVal >>
+--  print maybeEvaluatedTerm >>
+--  print ( TermWithContext ctx <$> maybeEvaluatedTerm )
+--  where
+--    ctx = ["p1", "p2"]
+--    term = parse "p1 p2"
+--    desugaredTerm = desugar term
+--    maybeNamelessTerm = removeNames ctx desugaredTerm
+--    maybeEvaluatedTerm = maybeNamelessTerm >>= eval
+--    isVal = isval <$> maybeNamelessTerm
