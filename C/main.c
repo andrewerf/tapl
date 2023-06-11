f76c6453a4e7c0c758a6266a008223477e875881
Andrew <andrew-aralov@yandex.ru>
2023-06-12 02:32:21 +0300

    Specify stubdir and include relatively.

--- C/main.c
+++ C/main.c
@@ -1,4 +1,4 @@
-#include "/home/andrew/Projects/haskell/tapl/.stack-work/dist/x86_64-linux-tinfo6/Cabal-3.4.1.0/build/Lambda2/Lambda2_stub.h"
+#include "Lambda2/Lambda2_stub.h"
 #include <stdio.h>
 
 int main()
